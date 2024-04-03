8bca99a8ee3b4acc97a1f4240f45700aea87b1af
Giovani Guizzo <giovani.guizzo_ext@yubico.com>
2024-04-03 16:45:33 -0300

    new branches

--- python/util/util.py
+++ python/util/util.py
@@ -3,7 +3,9 @@ def util_function(a: int, b: int) -> str:
         return "a < b"
     elif a > b:
         return "a > b"
-    elif a + b == 2:
-        return "a + b == 2"
+    elif a + b == 5:
+        return "a + b == 5"
+    elif a + b == 10:
+        return "a + b == 10"
     else:
         return "a === b"
