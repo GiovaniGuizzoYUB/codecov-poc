feb620219767e9d7c6e546921e3d655149a9d911
Giovani Guizzo <giovani.guizzo_ext@yubico.com>
2024-04-03 14:36:41 -0300

    add new test

--- typescript/src/App.test.tsx
+++ typescript/src/App.test.tsx
@@ -7,3 +7,9 @@ test("renders learn react link", () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test("renders learn react link with false flag", () => {
+  render(<App flag={false} />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
