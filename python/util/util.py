9730d2d11cbd33f4b934165861ef7eabcae9905a
Giovani Guizzo <giovani.guizzo_ext@yubico.com>
2024-04-03 16:38:24 -0300

    commit, failing test

--- python/util/util.py
+++ python/util/util.py
@@ -3,5 +3,7 @@ def util_function(a: int, b: int) -> str:
         return "a < b"
     elif a > b:
         return "a > b"
+    elif a + b == 2:
+        return "a + b == 2"
     else:
         return "a === b"
