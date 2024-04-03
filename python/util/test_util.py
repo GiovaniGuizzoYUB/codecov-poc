0a01d0ef2d538d1dfc745c8cc708cb2ddcf21f98
Giovani Guizzo <giovani.guizzo_ext@yubico.com>
2024-04-03 16:05:32 -0300

    feat: add python project

---
+++ python/util/test_util.py
@@ -0,0 +1,17 @@
+from .util import util_function
+import pytest
+
+
+def test_util_function_1():
+    result = util_function(1, 2)
+    assert result == "a < b", "Test case where a is less than b failed."
+
+
+def test_util_function_2():
+    result = util_function(2, 1)
+    assert result == "a > b", "Test case where a is greater than b failed."
+
+
+def test_util_function_3():
+    result = util_function(1, 1)
+    assert result == "a === b", "Test case where a is equal to b failed."
---
+++ python/util/util.py
@@ -0,0 +1,7 @@
+def util_function(a: int, b: int) -> str:
+    if a < b:
+        return "a < b"
+    elif a > b:
+        return "a > b"
+    else:
+        return "a === b"
