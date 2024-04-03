53bde74e882887cd4fb2851491542ec983cf1056
Giovani Guizzo <giovani.guizzo_ext@yubico.com>
2024-04-03 16:51:39 -0300

    change ts

--- typescript/src/util/utils.ts
+++ typescript/src/util/utils.ts
@@ -3,6 +3,10 @@ export function testUtilFunction(a: number, b: number) {
     return "a < b";
   } else if (a > b) {
     return "a > b";
+  } else if (a + b === 5) {
+    return "a + b == 5";
+  } else if (a + b === 10) {
+    return "a + b == 10";
   } else {
     return "a === b";
   }
