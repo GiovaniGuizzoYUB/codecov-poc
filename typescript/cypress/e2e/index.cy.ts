15ecc3f86cf2c0ed06ed016d23cc174fb46b2eed
Giovani Guizzo <giovani.guizzo_ext@yubico.com>
2024-04-16 08:22:49 -0300

    feat: add cypress

---
+++ typescript/cypress/e2e/index.cy.ts
@@ -0,0 +1,17 @@
+describe("template spec", () => {
+  it("visits page", () => {
+    cy.visit("/");
+  });
+
+  it("input a number", () => {
+    cy.visit("/");
+    cy.get("[data-testid=input-field]").clear().type("10");
+    cy.get("[data-testid=secret-text]").should("not.exist");
+  });
+
+  it("input a secret", () => {
+    cy.visit("/");
+    cy.get("[data-testid=input-field]").clear().type("secret");
+    cy.get("[data-testid=secret-text]").should("exist");
+  });
+});
