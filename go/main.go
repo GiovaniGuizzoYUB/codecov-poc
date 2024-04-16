a672360fd0d650bccd01047b64be68707089503a
Giovani Guizzo <giovani.guizzo_ext@yubico.com>
2024-04-16 11:51:27 -0300

    feat: add go

---
+++ go/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+// compareNumbers takes two integers, a and b, and returns a string describing their relationship.
+func compareNumbers(a int, b int) string {
+	if a < b {
+		return "a < b"
+	} else if a > b {
+		return "a > b"
+	} else if a+b == 5 {
+		return "a + b == 5"
+	} else if a+b == 10 {
+		return "a + b == 10"
+	} else {
+		return "a === b"
+	}
+}
+
+func main() {
+	fmt.Println(compareNumbers(3, 2)) // Example usage
+}
