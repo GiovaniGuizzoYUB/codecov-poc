351a21a626dbbc24a2f855ca40cce3f8cab1d7ef
Giovani Guizzo <giovani.guizzo_ext@yubico.com>
2024-04-23 17:35:51 -0300

    feat: add triangle to GO

---
+++ go/triangle.go
@@ -0,0 +1,20 @@
+package main
+
+func triangleType(a, b, c int) string {
+	// Validate non-zero positive lengths
+	if a <= 0 || b <= 0 || c <= 0 {
+		return "not a triangle"
+	}
+	// Check if it can form a triangle
+	if a+b <= c || a+c <= b || b+c <= a {
+		return "not a triangle"
+	}
+	// Determine the type of triangle
+	if a == b && b == c {
+		return "equilateral"
+	}
+	if a == b || b == c || a == c {
+		return "isosceles"
+	}
+	return "scalene"
+}
