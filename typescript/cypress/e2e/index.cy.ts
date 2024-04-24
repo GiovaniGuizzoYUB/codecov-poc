0c7c8d927dacdb4c9bb96e8ee4c7dae8fdc81186
Giovani Guizzo <giovani.guizzo_ext@yubico.com>
2024-04-24 09:13:45 -0300

    feat: add new tests

--- typescript/cypress/e2e/index.cy.ts
+++ typescript/cypress/e2e/index.cy.ts
@@ -9,15 +9,15 @@ describe("template spec", () => {
     cy.get("[data-testid=secret-text]").should("not.exist");
   });
 
-  // it("input a secret", () => {
-  //   cy.visit("/");
-  //   cy.get("[data-testid=input-field]").clear().type("secret");
-  //   cy.get("[data-testid=secret-text]").should("exist");
-  // });
+  it("input a secret", () => {
+    cy.visit("/");
+    cy.get("[data-testid=input-field]").clear().type("secret");
+    cy.get("[data-testid=secret-text]").should("exist");
+  });
 
-  // it("query params test", () => {
-  //   cy.visit("/?flag1=true&flag2=true");
-  //   cy.get("[data-testid=input-field]").clear().type("not a secret");
-  //   cy.get("[data-testid=secret-text]").should("not.exist");
-  // });
+  it("query params test", () => {
+    cy.visit("/?flag1=true&flag2=true");
+    cy.get("[data-testid=input-field]").clear().type("not a secret");
+    cy.get("[data-testid=secret-text]").should("not.exist");
+  });
 });
