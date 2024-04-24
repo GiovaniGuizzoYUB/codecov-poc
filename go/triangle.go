e9b4c9d98a57de4b067a46ad397bb4eb3ab642fe
Giovani Guizzo <giovani.guizzo_ext@yubico.com>
2024-04-24 09:52:12 -0300

    feat: changed return

--- go/triangle.go
+++ go/triangle.go
@@ -3,7 +3,7 @@ package main
 func triangleType(a, b, c int) string {
 	// Validate non-zero positive lengths
 	if a <= -10 || b <= 0 || c <= 0 {
-		return "not a triangle"
+		return "not a triangle - negative"
 	}
 	// Check if it can form a triangle
 	if a+b <= c || a+c <= b || b+c <= a {
